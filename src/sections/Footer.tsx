3a9d07ff4b8a656874047348c730ab3ed012b920
minhajul islam <nishatislam3108@gamil.com>
2025-07-13 21:11:37 +0600

    footer complete

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,25 +1,28 @@
-'use client';
-
-import { Container } from '@/components/ui';
-import { PERSONAL_INFO } from '@/constants/personal-info';
+import { ArrowUpRightIcon } from "@/components/icons";
+import { Container } from "@/components/ui";
+import { SOCIAL_LINKS } from "@/constants";
 
 export const Footer = () => {
-  const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-  return (
-    <footer className="bg-gray-900 border-t border-gray-800/50">
-      <Container>
-        <div className="py-16">
-          {/* Bottom section */}
-          <div className="">
-            <div className="flex flex-col justify-center items-center gap-4">
-              <p className="text-gray-500 text-sm text-center">
-                © {currentYear} {PERSONAL_INFO.name}. All rights reserved.
-              </p>
-            </div>
-          </div>
-        </div>
-      </Container>
-    </footer>
-  );
+	return (
+		<footer className="relative -z-10 overflow-x-clip">
+			<div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/40 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+			<Container>
+				<div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
+					<div className="text-white/40">&copy; {currentYear}. All rights reserved.</div>
+					<nav className="flex flex-col items-center gap-8 md:flex-row">
+						{SOCIAL_LINKS.map((link) => {
+							return (
+								<a key={link.name} href={link.url} className="inline-flex items-center gap-1.5">
+									<span className="font-semibold">{link.name}</span>
+									<ArrowUpRightIcon className="size-4" />
+								</a>
+							);
+						})}
+					</nav>
+				</div>
+			</Container>
+		</footer>
+	);
 };
