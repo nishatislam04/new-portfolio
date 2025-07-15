83ad77d62a72bf14cbdb7ccd61d897d5a33ef732
minhajul islam <nishatislam3108@gamil.com>
2025-07-15 13:26:31 +0600

    update footer link open in new tab

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
 	const currentYear = new Date().getFullYear();
 
 	return (
-		<footer className="relative -z-10 overflow-x-clip">
+		<footer className="relative z-20 overflow-x-clip">
 			<div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/40 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
 			<Container>
 				<div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
@@ -14,7 +14,7 @@ export const Footer = () => {
 					<nav className="flex flex-col items-center gap-8 md:flex-row">
 						{SOCIAL_LINKS.map((link) => {
 							return (
-								<a key={link.name} href={link.url} className="inline-flex items-center gap-1.5">
+								<a key={link.name} href={link.url} target="_blank" className="inline-flex items-center gap-1.5">
 									<span className="font-semibold">{link.name}</span>
 									<ArrowUpRightIcon className="size-4" />
 								</a>
