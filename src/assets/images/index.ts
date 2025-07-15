302e1bb83f579ce2b80201678bc1acf8a2bb965c
minhajul islam <nishatislam3108@gamil.com>
2025-07-15 18:10:56 +0600

    remove unused assets

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,48 +1,17 @@
-// Project images
-import aiStartupLandingPage from "./ai-startup-landing-page.png";
-import bookCover from "./book-cover.png";
-import darkSaasLandingPage from "./dark-saas-landing-page.png";
-import lightSaasLandingPage from "./light-saas-landing-page.png";
-
 // Work cover images
 import organizationCover from "../workCoverImages/organization_cover.jpg";
 import juktoCover from "../workCoverImages/jukto_cover.png";
 import teamDocsCover from "../workCoverImages/teamDocsCoverPhoto11.png";
 
 // Memoji avatars
-import memojiAvatar1 from "./memoji-avatar-1.png";
-import memojiAvatar2 from "./memoji-avatar-2.png";
-import memojiAvatar3 from "./memoji-avatar-3.png";
-import memojiAvatar4 from "./memoji-avatar-4.png";
-import memojiAvatar5 from "./memoji-avatar-5.png";
-import memojiComputer from "./memoji-computer.png";
 import memojiSmile from "./memoji-smile.png";
 
-// Other images
-import grain from "./grain.jpg";
-import map from "./map.png";
-
 export const projectImages = {
-	aiStartupLandingPage,
-	bookCover,
-	darkSaasLandingPage,
-	lightSaasLandingPage,
 	organizationCover,
 	juktoCover,
 	teamDocsCover,
 };
 
 export const memojiImages = {
-	avatar1: memojiAvatar1,
-	avatar2: memojiAvatar2,
-	avatar3: memojiAvatar3,
-	avatar4: memojiAvatar4,
-	avatar5: memojiAvatar5,
-	computer: memojiComputer,
 	smile: memojiSmile,
-};
-
-export const otherImages = {
-	grain,
-	map,
 };
